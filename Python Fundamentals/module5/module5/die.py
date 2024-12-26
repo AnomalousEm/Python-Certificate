f06d0fb4569010f6f9301e987080771a71629a16
Emily Shader <55066214+AnomalousEm@users.noreply.github.com>
2024-12-25 23:52:04 -0500

    Completed python fundamentals work

---
+++ Python Fundamentals/module5/module5/die.py
@@ -0,0 +1,10 @@
+"""
+A simple die roller
+
+Author: Emily Shader
+Date: December 24,2024
+"""
+
+import random
+
+roll = random.randint(1,6)
---
+++ Python Fundamentals/module5/module5/unit2/module.py
@@ -0,0 +1,14 @@
+"""
+A simple module.
+
+This file shows how modules work
+
+Author: Walker M. White (wmw2)
+Date:   July 31, 2018
+"""
+
+x = 1+2    # I am a comment
+x = 3*x
+
+y = x+500
+# Add your edits here
